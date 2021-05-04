ecd89655751db639d80bca685ab9bf052ecf9d2d
Mafe Bracho <mafe.bracho@gmail.com>
2021-05-04 15:30:13 +0200

    first commit

---
+++ ByteCoin/Model/CoinData.swift
@@ -0,0 +1,13 @@
+//
+//  CoinData.swift
+//  ByteCoin
+//
+//  Created by María Fernanda Bracho Salazar on 20.08.20.
+//  Copyright © 2020 The App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct CoinData: Codable {
+    let rate: Double
+}
